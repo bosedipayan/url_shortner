c6b3497fbfb1065797b50b1ccfcb8e4ff12d77ef
Dipayan Bose <bosedipayan2000@gmail.com>
2024-12-13 00:19:21 +0530

    Added modifications

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 app.use('/url', urlRoute);
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const Url = require('../models/url');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    return res.render("home");
+router.get("/", async (req, res) => {
+    const allUrls = await Url.find({});
+    return res.render("home", {
+        urls: allUrls
+    });
 })
 
 module.exports = router;
