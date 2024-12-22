6afd7d0f9cdf82ff47f4f3c965334ac13acaaf96
Dipayan Bose <bosedipayan2000@gmail.com>
2024-12-22 12:48:34 +0530

    Just a modification

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { v4: uuidv4 } = require('uuid');
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
@@ -8,9 +9,28 @@ async function handleUserSignup(req, res) {
         password
     });
 
-    return res.render("home");
+    return res.render("/");
+}
+
+async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+        email,
+        password
+    });
+
+    if (!user) {
+        return res.render("login", {
+            error: "Invalid username or password",
+        })
+    }
+
+    const sessionId = uuidv4();
+
+    return res.redirect("/");
 }
 
 module.exports = {
-    handleUserSignup
+    handleUserSignup,
+    handleUserLogin
 }
