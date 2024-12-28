87116041747a0d93324c5f797a3247d25a5044a8
Dipayan Bose <bosedipayan2000@gmail.com>
2024-12-28 18:51:32 +0530

    Just a modification

--- service/auth.js
+++ service/auth.js
@@ -1,7 +1,11 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+const secret = "Db_123";
+
+// const sessionIdToUserMap = new Map();
 
 function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+    // sessionIdToUserMap.set(id, user);
+    return jwt.sign(user, secret);
 }
 
 function getUser(id) {
