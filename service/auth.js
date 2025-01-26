f8b92d35d97f6d1db99cce1c5c858cd997c200ae
Dipayan Bose <bosedipayan2000@gmail.com>
2025-01-26 22:26:15 +0530

    modified

--- service/auth.js
+++ service/auth.js
@@ -3,13 +3,25 @@ const secret = "Db_123";
 
 // const sessionIdToUserMap = new Map();
 
-function setUser(id, user) {
+function setUser(user) {
     // sessionIdToUserMap.set(id, user);
-    return jwt.sign(user, secret);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email
+    }, secret);
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    // return sessionIdToUserMap.get(id);
+    if(!token) return null;
+
+    try{
+        return jwt.verify(token, secret);
+    }
+    catch(err){
+        console.error("Invalid token", err);
+        return null;
+    }
 }
 
 module.exports = {
