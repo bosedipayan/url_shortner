48aed105185d731d1903aa2452140b446ececf17
Dipayan Bose <bosedipayan2000@gmail.com>
2025-02-14 13:23:42 +0530

    Changed

--- controllers/user.js
+++ controllers/user.js
@@ -27,9 +27,10 @@ async function handleUserLogin(req, res) {
     }
 
     const token = setUser(user);
-    res.cookie("uid", token);
+    // res.cookie("uid", token);
+    return res.json({ token })
 
-    return res.redirect("/");
+    // return res.redirect("/");
 }
 
 module.exports = {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,21 @@
 const { getUser } = require('../service/auth');
 
+function checkForAuthentication(req, res, next) {
+    const authorizationHeaderValue = req.headers["Authorization"];
+    req.users = null;
+    if(!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer"))
+        return next();
+}
+
 async function restrictToLoggedinUserOnly(req, res, next) {
     // console.log(req);
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers["Authorization"];
 
     if (!userUid) {
         return res.redirect("/login");
     }
 
+    const token = userUid.split("Bearer ")[1];
     const user = getUser(userUid);
     if (!user) {
         return res.redirect("/login");
